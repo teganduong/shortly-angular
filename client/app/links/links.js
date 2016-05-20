c11778482975c95290f499c7c5f30dce6e532d9f
teganduong <teganduong@gmail.com>
2016-05-20 15:11:58 -0700

    Implement LinksController

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,9 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
+  $scope.data = {};
+  Links.getAll().then(function(links) {
+    $scope.data.links = links;
+  });
 
 });
