b9dc88c3d7c7159ca06cbabffc2939a9c90fa89e
teganduong <teganduong@gmail.com>
2016-05-20 20:26:07 -0700

    Implement logout button in nav

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,10 +1,15 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
+.controller('LinksController', function ($scope, Links, Auth) {
   $scope.data = {};
   $scope.query = {};
   $scope.queryBy = '$';
+  
   Links.getAll().then(function(links) {
     $scope.data.links = links;
   });
+
+  $scope.signout = function() {
+    Auth.signout();
+  };
 });
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,9 +1,13 @@
 angular.module('shortly.shorten', [])
 
-.controller('ShortenController', function ($scope, $location, Links) {
+.controller('ShortenController', function ($scope, $location, Links, Auth) {
   $scope.link = {};
 
+  $scope.signout = function() {
+    Auth.signout();
+  };
 
+  
   $scope.addLink = function() {
     Links.addOne($scope.link)
     .then(function() {
